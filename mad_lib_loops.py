ddcfbe1bd12f5643e64f729a40f2aad76069028f
Aidan Farhi <aidanmcfarhi@gmail.com>
2023-05-25 22:47:36 -0400

    finished mad_lib_loops

---
+++ mad_lib_loops.py
@@ -0,0 +1,27 @@
+"""
+Mad Libs are activities that have a person provide various words,
+which are then used to complete a short story in unexpected (and hopefully funny) ways.
+
+Write a program that takes a string and an integer as input, and outputs 
+a sentence using the input values as shown in the example below. The program 
+repeats until the input string is quit and disregards the integer input that follows.
+
+Ex: If the input is:
+
+apples 5
+shoes 2
+quit 0
+the output is:
+
+Eating 5 apples a day keeps the doctor away.
+Eating 2 shoes a day keeps the doctor away.
+"""
+
+# get input text from standard input
+input_text = input()
+
+# keep looping and printing result to standard output until 'quit' is recieved from standard input
+while 'quit' not in input_text:
+    word, number = input_text.split(' ')
+    print(f'Eating {number} {word} a day keeps the doctor away.')
+    input_text = input()
